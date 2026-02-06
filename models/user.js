b9f0ad1791a08e3734e9a37a661b7ba36e242f83
Sierra07-w <mukhammadsara07@gmail.com>
2026-02-06 13:27:15 +0500

    Add user creation function with password hashing

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const bcrypt=require("bcrypt")
+
+async function createUser(db,email,password){
+
+const hash=await bcrypt.hash(password,10)
+
+await db.collection("users").insertOne({
+email,
+password:hash
+})
+
+}
+
+module.exports={createUser}
